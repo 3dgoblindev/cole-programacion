d02964e34a51b31a00de6ac383370e4b8f4e0110
3DGoblinDev <lorenax230403@gmail.com>
2025-11-23 21:36:41 +0100

    a

---
+++ Unidad6/Unidad6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic; // <- necesario para List<T>
+
+class Program
+{
+    static void Main()
+    {
+        //Crear una lista y añadir elementos:
+        List<int> primes = new List<int>();
+        primes.Add(2);
+        primes.Add(3);
+        primes.Add(5);
+        primes.Add(7);
+
+        //Iterar con for:
+        for (int i = 0; i < primes.Count; i++)
+        {
+            Console.WriteLine(primes[i]);
+        }
+
+        //Iterar con foreach:
+        foreach (int p in primes)
+        {
+            Console.WriteLine("Prime: " + p);
+        }
+
+        //Insertar en una posición concreta:
+        primes.Insert(1, 11); //insertamos en la posición 1
+        Console.WriteLine("Después de insertar 11: " + string.Join(", ", primes));
+
+        //Borrar un elemento:
+        primes.Remove(11);
+        Console.WriteLine("Después de borrar 11: " + string.Join(", ", primes));
+
+        //Comprobar si existe un elemento:
+        Console.WriteLine(primes.Contains(5)); // true
+        Console.WriteLine(primes.IndexOf(7));  // índice de 7
+
+        //Ordenar una lista:
+        primes.Sort();
+        Console.WriteLine("Ordenada: " + string.Join(", ", primes));
+
+        //Vaciar la lista:
+        List<string> colors = new List<string>() { "rojo", "verde", "azul" };
+        colors.Clear();
+        Console.WriteLine("Colores tras Clear(): " + colors.Count);
+
+
+        //Obtener un elemento aleatorio:
+        List<string> fruits = new List<string>() { "manzana", "pera", "uva", "plátano" };
+        Random rnd = new Random();
+        int index = rnd.Next(fruits.Count);
+        Console.WriteLine("Fruta aleatoria: " + fruits[index]);
+
+
+
+    }
+}
---
+++ Unidad7/Unidad7/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Ejemplo de uso según el PDF:
+
+        Pokemon uno = new Pokemon(10, 2, "Jose Antonio");
+        Pokemon otro = new Pokemon(20, 3, "Maria Luisa");
+
+        Pikachu pi = new Pikachu(10, 3, "El de ash");
+        Pokemon pi2 = new Pikachu(20, 2, "El detective");
+
+        pi.Gritar();
+
+        //pi2.Gritar(); //ERROR: la clase Pokemon no tiene Gritar()
+
+        ((Pikachu)pi2).Gritar(); //casting para llamar a Gritar
+    }
+}
+
+
+//-------------------------------
+//Clase base
+//-------------------------------
+class Pokemon
+{
+    protected int ataque;
+    protected int vida;
+    protected string nombre;
+
+    public Pokemon(int vida, int ataque, string nombre)
+    {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.nombre = nombre;
+    }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Vida: " + vida);
+        Console.WriteLine("Ataque: " + ataque);
+    }
+}
+
+
+//-------------------------------
+//Clase derivada 
+//-------------------------------
+class Pikachu : Pokemon
+{
+    public Pikachu(int vida, int ataque, string nombre) : base(vida, ataque, nombre)
+    {
+    }
+
+    public void Gritar()
+    {
+        Console.WriteLine("Pikaaaa");
+        Console.WriteLine("ah, y tengo " + this.vida + " de vida");
+    }
+}
